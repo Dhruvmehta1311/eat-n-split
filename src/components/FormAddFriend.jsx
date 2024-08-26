858b523b590e16a4d8c19f65dc7f6313913d8ab1
Dhruv Mehta <dhruvmehta1311@gmail.com>
2024-08-26 21:43:30 +0530

    Minor Changes in form

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -7,12 +7,20 @@ const FormAddFriend = () => {
       <form className="flex flex-col gap-4 font-semibold text-lg max-w-[full] mx-auto w-[99%]">
         <section className="flex  flex-col justify-between gap-2 ">
           <label>Friend&apos;s Name: </label>
-          <input type="text" className="max-w-80 h-[40px] rounded" />
+          <input
+            placeholder="Enter Friend's Name"
+            type="text"
+            className="max-w-80 h-[40px] rounded pl-4 outline-none"
+          />
         </section>
 
         <section className="flex flex-col justify-between gap-2 ">
           <label>Image URL: </label>
-          <input type="text" className="max-w-80 h-[40px] rounded" />
+          <input
+            placeholder="Image URL Here"
+            type="text"
+            className="max-w-80 h-[40px] rounded pl-4 outline-none"
+          />
         </section>
         <div className="flex sm:justify-end">
           <Button>Close</Button>
